bd38ff057096b8919e84649a5e53b12e402f53fa
chloejepson <jepsoc@rpi.edu>
2023-10-24 09:32:05 -0400

    finishing form/enndpoint

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app.jsx",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", "./routes/postAccident.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
