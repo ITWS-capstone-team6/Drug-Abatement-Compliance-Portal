6e6b493e022a95121c7db00283549c4af93420fd
chloejepson <jepsoc@rpi.edu>
2023-11-13 13:40:26 -0500

    adding sign up versus sign in

--- Frontend/src/routes/signUp.js
+++ Frontend/src/routes/signUp.js
@@ -20,31 +20,21 @@ export default function Login() {
     function handleLogin(e) {
         e.preventDefault();
         console.log(loggedIn);
-        
-        var authenticationData={
-            Username: email,
-            Password: password
-        }
-        console.log(authenticationData)
+        // To-do: validate credentials
         var userData={
             Username: email,
             Pool: UserPool
         }
-        console.log(userData)
         var cognitoUser= new AmazonCognitoIdentity.CognitoUser(userData);
-        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-        cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: function (result){
-                console.log('user credentials have been authenticated')
-                //potentially use this to log in db
-                var idToken= result.getIdToken().getJwtToken();
-                getCognitoIdentityCredentials();
-            },
-            onFailure: function(err){
-                console.log(err.message);
+        
+        UserPool.signUp(email, password, [], null, (err, data)=> {
+            if(err){
+                console.log(err);
+            }
+            else{
+                const confirmationCode= prompt("Please enter the confirmation code that has been sent to your email.")
             }
         })
-        setLoggedIn(true);
     };
 
     return <>
@@ -55,7 +45,7 @@ export default function Login() {
             <form class="px-8" onSubmit={handleLogin}>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
+                        Email
                     </label>
                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={email} htmlFor="username" id="username" type="text" placeholder="Username" reqquired onChange={(event)=> setEmail(event.target.value)}/>
                 </div>
@@ -65,15 +55,17 @@ export default function Login() {
                     </label>
                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" value={password} htmlFor="password" id="password" type="password" placeholder="***********" required onChange={(event)=> setPassword(event.target.value)}/>
                 </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Confirm Password
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" value={password} htmlFor="password" id="password" type="password" placeholder="***********" required onChange={(event)=> setPassword(event.target.value)}/>
+                </div>
                 <div class="flex items-center justify-between">
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Sign In
+                        Sign Up
                     </button>
-                    
                 </div>
-                <span class="text-sm text-blue-500 underline cursor-pointer" onClick={() => alert("Redirect to Sign Up page")}>
-                    Sign Up
-                </span>
             </form>
         </div>
     </div>
