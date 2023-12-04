374c6249ba575c7c1a3319f7fbbde95b9ad50aa4
James Liu <liuxjames88@gmail.com>
2023-12-04 17:07:56 -0500

    rework the layout and routing to be better

--- Frontend/src/routes/layout.jsx
+++ Frontend/src/routes/layout.jsx
@@ -1,58 +1,46 @@
-import { useNavigate, Outlet } from "react-router-dom";
+import { useNavigate, Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/navbar";
 import './layout.css'
-import Login from "./login";
-import SignUp from "./signUp";
 
 import { useAtom } from 'jotai';
-import { loggedInAtom, loginStateAtom } from '../state/login';
-import {userIdAtom, userEmailAtom, userAwsUserIdAtom, userIsAdminAtom} from '../state/userInfo';
+import { loggedInAtom } from '../state/login';
+import { userAwsUserIdAtom, userIsAdminAtom} from '../state/userInfo';
 import { useEffect } from "react";
-import AdminDashboard from "./adminDashboard";
-
-
-const view = {
-  LOGIN: true,
-  SIGNUP: false,
-};
 
 export default function Layout() {
 
 
   const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
-  console.log("logged in: " + loggedIn)
-  const [loginState] = useAtom(loginStateAtom);
 
-  const [userId] = useAtom(userIdAtom);
   const [awsUserId] = useAtom(userAwsUserIdAtom);
-  const [userEmail]= useAtom(userEmailAtom);
   const [userIsAdmin, setUserIsAdmin] = useAtom(userIsAdminAtom);
 
+  const navigate = useNavigate();
+  const location = useLocation();
 
 
   useEffect(() => {
+    if (!loggedIn && location !== "/login" && location !== "/signUp") {
+
+      console.log("user is not logged in")
+      navigate("/login")
+    }
+  }, [loggedIn, userIsAdmin, navigate, location]);
+
+  useEffect(() => {
     if (loggedIn) {
-      console.log("user is now logged in. this is where we would query for the user info from mongo now")
       // fetch call to api with the query being the user id
       //  api returns user info from database
       // query api to see the user info
-      fetch(`http://localhost:5000/isAdmin?`+ new URLSearchParams({userId: awsUserId})).then((response) => {
-        console.log("got response from fetch call in layout.jsx")
+      fetch(`http://localhost:5000/isAdmin?`+ new URLSearchParams({userId: awsUserId}))
+      .then((response) => {
         return response.json();
       }).then((data) => {
-
-        console.log(data)
         setUserIsAdmin(data);
-        if (data) {
-          console.log("user is admin")
-        } else {
-          console.log("user is not admin")
-        }
       })
     }
-  }, [loggedIn, awsUserId]);
+  }, [loggedIn, awsUserId, setUserIsAdmin]);
 
-  const navigate= useNavigate();
 
   const test = () => {
     console.log("test")
@@ -68,18 +56,11 @@ export default function Layout() {
   return (
     <>
       <div className="main-content">
-        {
-          !loggedIn && (loginState === view.LOGIN ? <Login /> : <SignUp/>)
-        }
-        {(loggedIn) &&
-          <>
-          <Navbar />
-          {userIsAdmin ? <AdminDashboard /> : <Outlet />}
-          {/* <Outlet /> */}
-          <button onClick={test}>test</button>
-          <button onClick={logOut}>log out</button>
-          </>
-        }
+        { loggedIn && <Navbar /> }
+        <Outlet />
+
+        <button onClick={test}>test</button>
+        <button onClick={logOut}>log out</button>
       </div>
     </>
   );
